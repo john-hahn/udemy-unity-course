1c95e109a3dabf086248f67ffa3e2e4cdd28095b
John Hahn <johnjhhahn@gmail.com>
2021-08-26 13:51:26 +0900

    Added audio source to rocket

--- 2. Space Odyssey/Assets/Scripts/Movement.cs
+++ 2. Space Odyssey/Assets/Scripts/Movement.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    Rigidbody rb;
     [SerializeField] float thrustRate = 1000f;
     [SerializeField] float rotationRate = 1000f;
+    Rigidbody rb;
+    AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -26,6 +28,14 @@ public class Movement : MonoBehaviour
         if (Input.GetKey(KeyCode.Space))
         {
             rb.AddRelativeForce(Vector3.up * thrustRate * Time.deltaTime);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
         }
     }
 
@@ -44,6 +54,8 @@ public class Movement : MonoBehaviour
 
     void ApplyRotation(float rotationThisFrame) 
     {
+        rb.freezeRotation = true;
         transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
+        rb.freezeRotation = false;
     }
 }
